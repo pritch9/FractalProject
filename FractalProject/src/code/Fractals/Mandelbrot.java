643b2630ce3c14c0d56db16a453d470c1044cca6
Will <Will@MSI>
2017-02-17 12:06:05 -0500

    Commented a bunch

--- FractalProject/src/code/Fractals/Mandelbrot.java
+++ FractalProject/src/code/Fractals/Mandelbrot.java
@@ -2,13 +2,53 @@ package code.Fractals;
 
 public class Mandelbrot {
 	
+	/**
+	 * Max amount of passes
+	 */
 	private int _max;
 	
 	/**
-	 * Automagically sets the max to 255.  Max can be set with setMax(int max)
+	 * Dimensions of points
+	 */
+	private int _rows, _cols;
+	
+	/**
+	 * Bounds
+	 */
+	private double _upperX,_lowerX, _upperY, _lowerY;
+	
+	/**
+	 * Initialize with defaults:
+	 * Max Passes: 255
+	 * Bounds: 	X: [-2.15,0.6]
+	 * 			Y: [-1.3,1.3]
 	 */
 	public Mandelbrot(){
-		_max = 255;
+		this(512, 512);
+	}
+	
+	/**
+	 * Set rows and columns upon construction
+	 * 
+	 * @param rows amount of rows in points array
+	 * @param cols amount of columns in points array
+	 */
+	public Mandelbrot(int rows, int cols){
+		_max = 255; // Default max passes
+		
+		// Defaults points dimensions to 512
+		_rows = rows;
+		_cols = cols;
+		
+		setBounds(0.6,-2.15,1.3,-1.3);
+		
+		// X bounds: [-2.15, 0.6]
+		_upperX = 0.6;
+		_lowerX = -2.15;
+		
+		// Y bounds: [-1.3, 1.3]
+		_upperY = 1.3;
+		_lowerY = -1.3;
 	}
 	
 	/**
@@ -17,35 +57,67 @@ public class Mandelbrot {
 	public void setMax(int max){
 		_max = max;
 	}
+	
+	/**
+	 * Sets the bounds of the set
+	 */
+	public void setBounds(double upperX, double lowerX, double upperY, double lowerY){
+		// The next two if statements makes sure the upper bound is > the lower bound and swaps them if needed
+		if(upperX < lowerX){
+			double t = upperX;
+			upperX = lowerX;
+			lowerX = t;
+		}
+		if(upperY < lowerY){
+			double t = upperY;
+			upperY = lowerY;
+			lowerY = t;
+		}
+		
+		// Sets all the instance variables
+		_upperX = upperX;
+		_lowerX = lowerX;
+		_upperY = upperY;
+		_lowerY = lowerY;
+	}
 
 	/**
 	 * Returns the 2-D array of points
 	 * @return int[][] points
 	 */
 	public int[][] getPoints() {
-		int[][] points = new int[512][512];
-		double xCalc, yCalc, tX, tY, tmp, dist;
-		for(int rows = 0; rows < 512; rows++){ // y
-			for(int cols = 0; cols < 512; cols++){ // x
+		int[][] points = new int[_cols][_rows]; // define the return array [X][Y]
+		
+		double xCalc, yCalc, tX, tY, tmp, dist; // define calculation local variables
+		
+		// The for loops iterate through the entire 2D array table
+		for(int rows = 0; rows < _rows; rows++){ // y
+			for(int cols = 0; cols < _cols; cols++){ // x
+				
 				// convert rows and cols to cartesian plot
-				xCalc = ((2.75) / 512.0) * cols - 2.15;
-				yCalc = ((2.6) / 512.0) * rows - 1.3;
+				xCalc = ((_upperX - _lowerX) / _cols) * cols + _lowerX;
+				yCalc = ((_upperY - _lowerY) / _rows) * rows + _lowerY;
+				
+				// Mandelbrot starts every test from (0,0) AKA the origin
 				tX = 0;
 				tY = 0;
 				
-				int passes = -1;
-				dist = Math.sqrt(tX*tX + tY*tY);
-				while(dist < 2 && passes < _max){
-					tmp = tX*tX - tY*tY + xCalc;
-					tY = 2.0 * tX * tY + yCalc;
-					tX = tmp;
-					dist = Math.sqrt(tX*tX + tY*tY);
-					passes++;
+				
+				int passes = -1; // Start passes at -1 because it passes once by default
+				
+				dist = 0; // (0,0) has a distance of 0 from (0,0)
+				
+				while(dist < 4 && passes < _max){
+					tmp = tX*tX - tY*tY + xCalc; // TEMP X: x' = x^2 - y^2 + (original x value)
+					tY = 2.0 * tX * tY + yCalc; // y' = 2xy + (original y value)
+					tX = tmp; // set X to TEMP X
+					dist = Math.sqrt(tX*tX + tY*tY); // recalculate distance
+					passes++; // add a pass
 				}
-				points[cols][rows] = passes;
+				points[cols][rows] = passes; // When the while loop escapes, set the passes in the points 2D array
 			}
 		}
-		return points;
+		return points;  // returned filled points array
 	}
 
 	/**\
