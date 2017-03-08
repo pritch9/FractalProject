693d5fcf5ae6b86218282c1a12d686b4b0158cca
Will <Will@MSI>
2017-03-08 18:42:06 -0500

    menu item example

--- FractalProject/src/code/UI/FractalViewer.java
+++ FractalProject/src/code/UI/FractalViewer.java
@@ -1,9 +1,40 @@
 package code.UI;
 
+import java.awt.event.KeyEvent;
+
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
 
 public class FractalViewer extends JFrame{
 	
-	
+	public FractalViewer(){
+		setupJMenuBar();
+		setupJFrame();
+	}
+
+	private void setupJFrame() {
+		
+	}
+
+	private void setupJMenuBar() {
+		
+		/*
+		
+		// Fractal menu
+		menu = new JMenu("Fractals"); // new JMenu
+		menu.setMnemonic(KeyEvent.VK_F); // not sure if this does anything
+		menu.getAccessibleContext().setAccessibleDescription("I scream for Ice Cream!"); // description for menu
+		
+		// fractal menu items
+		menuItem = new JMenuItem("Mandelbrot Set"); // Menu option text
+		menuItem.getAccessibleContext().setAccessibleDescription("Show the Mandelbrot Set in the viewer"); // menu option description
+		menuItem.addActionListener((e)->{ changeFractal(MANDELBROT); }); // action listener
+		menu.add(menuItem); // add the item to the menu
+
+		
+		*/
+		
+	}
 
 }
