5ef914a4efbf34376afcf036a5c30fcc2c8dda72
Will <Will@MSI>
2017-03-08 19:22:47 -0500

    commit

--- FractalProject/src/code/UI/FractalViewer.java
+++ FractalProject/src/code/UI/FractalViewer.java
@@ -1,23 +1,61 @@
 package code.UI;
 
+import java.awt.Toolkit;
 import java.awt.event.KeyEvent;
 
 import javax.swing.JFrame;
 import javax.swing.JMenu;
+import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
+import code.Fractals.BurningShip;
+import code.Fractals.Fractal;
+import code.Fractals.Julia;
+import code.Fractals.Mandelbrot;
+import code.Fractals.Multibrot;
+import edu.buffalo.fractal.FractalPanel;
 
 public class FractalViewer extends JFrame{
+	private JMenuBar _menuBar;
+	private Fractal[] _fractals;
+	private Fractal _current;
+	private FractalPanel _fractalPanel;
 	
 	public FractalViewer(){
+		setupFractals();
 		setupJMenuBar();
+		setupFractalPanel();
 		setupJFrame();
 	}
 
+	private void setupFractalPanel() {
+		_fractalPanel = new FractalPanel();
+		_fractalPanel.setIndexColorModel(ColorModelFactory.createGrayColorModel(50));
+		_fractalPanel.updateImage(_current.getPoints());
+		this.add(_fractalPanel);
+	}
+
 	private void setupJFrame() {
-		
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+			SwingUtilities.updateComponentTreeUI(this);
+		} catch (ClassNotFoundException | InstantiationException | IllegalAccessException
+				| UnsupportedLookAndFeelException e) {
+			e.printStackTrace();
+		}
+		this.pack();
+		this.setLocation((Toolkit.getDefaultToolkit().getScreenSize().width - this.getWidth()) / 2, (Toolkit.getDefaultToolkit().getScreenSize().height - this.getHeight()) / 2);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setResizable(false);
+		this.setVisible(true);
 	}
 
 	private void setupJMenuBar() {
+		
+		_menuBar = new JMenuBar();
 		
 		/*
 		
@@ -34,8 +72,20 @@ public class FractalViewer extends JFrame{
 
 		
 		*/
-
 		
+		
+		JMenu menu = new JMenu("Fractals");
+		menu.setMnemonic(KeyEvent.VK_F);
+		menu.getAccessibleContext().setAccessibleDescription("Change the fractal being viewed");
+		
+		for(Fractal f : _fractals){
+			JMenuItem i = new JMenuItem(f.getName());
+			i.getAccessibleContext().setAccessibleDescription("Show the " + f.getName() + " in the viewer");
+			i.addActionListener((e) -> { changeFractal(f); });
+			menu.add(i);
+		}
+		_menuBar.add(menu);
+
 		// Creates new menu item with description
 		JMenu colorBar = new JMenu("Colors");
 		colorBar.setMnemonic(KeyEvent.VK_F);
@@ -60,6 +110,24 @@ public class FractalViewer extends JFrame{
 		colorD.getAccessibleContext().setAccessibleDescription("Changes Fractals to color D");
 		colorD.addActionListener(null); //null for now
 		colorBar.add(colorD);
+		
+		_menuBar.add(colorBar);
+		
+		
+		this.setJMenuBar(_menuBar);
+	}
+	
+	private void changeFractal(Fractal f) {
+		_current = f;
+		
 	}
 
+	private void setupFractals(){
+		_fractals = new Fractal[4];
+		_fractals[0] = new Mandelbrot();
+		_fractals[1] = new Julia();
+		_fractals[2] = new BurningShip();
+		_fractals[3] = new Multibrot();
+		_current = _fractals[0];
+	}
 }
